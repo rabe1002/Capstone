9df087297a9980f66177228b56912cff7900677c
rabe1002 <43972549+rabe1002@users.noreply.github.com>
2019-11-19 17:32:04 -0600

    Created Home login page

---
+++ Capstone/Models/Item.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Capstone.Models
+{
+    public class Item
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+}
